2d926838d6860466b4a78dedf201440a482c1b8f
VgL <ve.guerrero7@gmail.com>
2021-07-04 16:56:52 -0400

    Lista de validacion comunas

--- Proyecto.py
+++ Proyecto.py
@@ -12,3 +12,13 @@ def listaComunasUsuario():
     menuComunas = str(comunasListadoUsuario)
     menuComunas = menuComunas.replace("'","").replace("[","").replace("]","")
     print(menuComunas)
+
+def listaComunasValidacion():
+    with open("Datos.csv","r",encoding="utf-8") as f:
+        listaParaValidacionComunas = []
+        for i in f.readlines():
+            linea = i.split(",")
+            if linea[2] != "Comuna" and len(linea[3])>0:
+                listaParaValidacionComunas.append(linea[2])
+                listaParaValidacionComunas.append(linea[3])
+    return listaParaValidacionComunas
