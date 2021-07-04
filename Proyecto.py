adbdc84c589fb24cd178402698862a2c97019d59
VgL <ve.guerrero7@gmail.com>
2021-07-04 18:42:10 -0400

    Comunas

--- Proyecto.py
+++ Proyecto.py
@@ -22,3 +22,21 @@ def listaComunasValidacion():
                 listaParaValidacionComunas.append(linea[2])
                 listaParaValidacionComunas.append(linea[3])
     return listaParaValidacionComunas
+
+def filtroDatosComuna(comuna):
+    with open("Datos.csv","r",encoding="utf-8") as f:
+        for i in f.readlines():
+            linea = i.split(",")
+            if comuna in linea:
+                datoFiltrado = []
+                for count in range(len(linea)-9,len(linea)-1):
+                    datoFiltrado.append(int(float(linea[count])))
+    return datoFiltrado
+
+def datoComunaNoAcumulado(datoFiltrado):
+    listaDatoNoAcumulado = []
+    for i in range(1,len(datoFiltrado)):
+        operacion = datoFiltrado[i] - datoFiltrado[i-1]
+        listaDatoNoAcumulado.append(int(operacion))
+    return listaDatoNoAcumulado        
+
